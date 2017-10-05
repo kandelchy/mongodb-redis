bfcfb9c4803cc8333e1affec5049637fffe3a4fd
kandelchy <kandelchy@gmail.com>
2017-10-05 12:48:33 -0500

    Add your test version

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,44 +7,61 @@ class PeopleController < ApplicationController
 
   def new
     @person = Person.new
-
-
-
   end
 
   def create
+    @person = Person.new(person_params)
+    # Make sure at least one slave is up and running beside Master`
+     while $REDIS.wait(2,1000) == 0
+       puts "Waiting for Slave"
+       sleep 10
+     end
 
-    data = (0..100).map do |i|
-      { :a =>"this message num#{i}"   }
-    end
-    $redis.set(:key , data)
+     $messageno=$messageno+1
+    #  puts $messageno
+    #  puts @person.message
+     puts $REDIS.set("Message: #{$messageno}", @person.message )
 
-    redis_value = $redis.get(:key)
-    pp "----------------------------------------"
+     redis_value = $REDIS.get("Message: #{$messageno}")
+     pp "----------------------------------------"
 
-    pp redis_value
+     pp redis_value
 
 
-   #@person = Person.create(message:redis_value)
-
-   @person = Person.new(message:redis_value)
+     @person = Person.new(message:redis_value)
 
      if @person.save
-
-       redirect_to root_path, notice: "The message has been save from redis to mongodb" and return
+       redirect_to root_path, notice: "The message has been saved from redis to mongodb" and return
      end
-  # if @person.save
-  #redirect_to root_path, notice: " has been updated! from redis" and return
-   #end
+
+
+     # data = (0..100).map do |i|
+     #   { :a =>"this message num#{i}"   }
+     # end
+     # $redis.set(:key , data)
+
+
+
+    #@person = Person.create(message:redis_value)
+
+
+   # if @person.save
+   #redirect_to root_path, notice: " has been updated! from redis" and return
+    #end
 
 
 
 
-
-
-  end
-
-
+  #  respond_to do |format|
+  #    if @person.save
+  #      format.html { redirect_to @person, notice: 'Upload was successfully created.' }
+  #      format.json { render :show, status: :created, location: @person }
+  #    else
+  #      format.html { render :new }
+  #      format.json { render json: @person.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+ end
 
   def person_params
     params.require(:person).permit(:message,:redis_value)
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,14 @@
-$redis = Redis::Namespace.new(:redis => Redis.new)
+# $redis = Redis::Namespace.new(:redis => Redis.new)
 #redis = Redis.new(host: "10.0.1.1", port: 6379, db:0)
+redis_options = {
+  url: 'redis://example-test',
+  sentinels: [ { host: "127.0.0.1", port: 26379 },
+               { host: "127.0.0.1", port: 26379 },
+               { host: "127.0.0.1", port: 26379 }],
+  role: 'master'
+}
+
+
+$REDIS = Redis.new(redis_options)
+$messageno=0
+puts $REDIS.set("number","***********SENDING MESSAGE No. #{$messageno} *********")
